757fb6d55f39a51546355ed19ab4a49543572a1e
Stanislav <stanisov@gmail.com>
2014-12-01 13:58:33 +0200

    Update config.php

--- app/modules/ZKZ/config/config.php
+++ app/modules/ZKZ/config/config.php
@@ -35,7 +35,7 @@
 			'adapter'     => 'Mysql',
 			'host'        => 'localhost',
 			'username'    => 'root',
-			'password'    => 'd9eb77mms',
+			'password'    => '',
 			'dbname'      => 'Shop',
 			'persistent'  => false
 		],
